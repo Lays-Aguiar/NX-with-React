38da58296fa16199451259c6398ce82b61e0476f
Lays-Aguiar <lays.aguiar.dev@gmail.com>
2022-08-29 16:23:04 -0300

    add and implementation test and component

--- apps/todos/src/app/app.tsx
+++ apps/todos/src/app/app.tsx
@@ -1,15 +1,36 @@
-import styled from 'styled-components';
-import NxWelcome from './nx-welcome';
+import { useState } from 'react';
+import * as S from './style';
 
-const StyledApp = styled.div`
-  // Your style here
-`;
+interface Todo {
+  title: string;
+}
 
 export function App() {
+  const [todos, setTodos] = useState<Todo[]>([
+    { title: 'Todo 1' },
+    { title: 'Todo 2' },
+  ]);
+
+  function addTodo() {
+    setTodos([
+      ...todos,
+      {
+        title: `New todo ${Math.floor(Math.random() * 1000)}`,
+      },
+    ]);
+  }
   return (
-    <StyledApp>
-      <NxWelcome title="todos" />
-    </StyledApp>
+    <S.Container>
+      <h1>Todos</h1>
+      <ul>
+        {todos.map((t) => (
+          <li className={'todo'}>{t.title}</li>
+        ))}
+      </ul>
+      <S.Button id={'add-todo'} onClick={addTodo}>
+        Add Todo
+      </S.Button>
+    </S.Container>
   );
 }
 
---
+++ apps/todos/src/app/style.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #f5f5f5;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  font: 500 1.125rem 'Raleway', sans-serif;
+`;
+
+export const Button = styled.button`
+  height: 2rem;
+  border-radius: 0.5rem;
+  border: none;
+  background-color: #044da1;
+  align-items: center;
+`;
